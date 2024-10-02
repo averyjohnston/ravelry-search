5fa4f213876ee443deb3f398e2983a7e743f636c
averyjohnston <averyejohnston@gmail.com>
2024-10-02 12:27:52 -0500

    chore(eslint): fix rules not actually being in rules obj (whoops)

--- eslint.config.js
+++ eslint.config.js
@@ -38,36 +38,36 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@stylistic/quotes': ['warn', 'single'],
+      '@stylistic/no-extra-parens': ['warn', 'all', {
+        ignoreJSX: 'multi-line'
+      }],
+      '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+      '@stylistic/jsx-quotes': 'warn',
+      '@stylistic/spaced-comment': 'warn',
+      '@stylistic/jsx-self-closing-comp': 'warn',
+      '@stylistic/jsx-wrap-multilines': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-base-to-string': ['warn', {
+        ignoredTypeNames: ['Error', 'FormDataEntryValue', 'RegExp', 'URL', 'URLSearchParams']
+      }],
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }],
+      'sort-imports': ['warn', {
+        ignoreDeclarationSort: true,
+        ignoreCase: true,
+      }],
+      'import/order': ['warn', {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true
+      }],
     },
-    '@stylistic/quotes': ['warn', 'single'],
-    '@stylistic/no-extra-parens': ['warn', 'all', {
-      ignoreJSX: 'multi-line'
-    }],
-    '@stylistic/comma-dangle': ['warn', 'always-multiline'],
-    '@stylistic/jsx-quotes': 'warn',
-    '@stylistic/spaced-comment': 'warn',
-    '@stylistic/jsx-self-closing-comp': 'warn',
-    '@stylistic/jsx-wrap-multilines': 'warn',
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    '@typescript-eslint/no-base-to-string': ['warn', {
-      ignoredTypeNames: ['Error', 'FormDataEntryValue', 'RegExp', 'URL', 'URLSearchParams']
-    }],
-    '@typescript-eslint/no-unused-vars': ['error', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_',
-      caughtErrorsIgnorePattern: '^_'
-    }],
-    'sort-imports': ['warn', {
-      ignoreDeclarationSort: true,
-      ignoreCase: true,
-    }],
-    'import/order': ['warn', {
-      'newlines-between': 'always',
-      alphabetize: {
-        order: 'asc',
-        caseInsensitive: true,
-      },
-      warnOnUnassignedImports: true
-    }],
   },
 )
