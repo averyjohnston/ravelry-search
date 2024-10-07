81cc7a25291bb45753cbd596adb42c8d5c8bf989
averyjohnston <averyejohnston@gmail.com>
2024-10-07 13:59:37 -0500

    feat(utils): add ability to pass params to get util

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,10 @@
-const { VITE_RAVELRY_USERNAME, VITE_RAVELRY_PASSWORD } = import.meta.env;
+const { VITE_RAVELRY_USERNAME, VITE_RAVELRY_PASSWORD, VITE_RAVELRY_ACCOUNT_NAME } = import.meta.env;
 
-export async function get(path: string) {
-  const response = await fetch(`https://api.ravelry.com${path}`, {
+export const USERNAME = VITE_RAVELRY_ACCOUNT_NAME as string | undefined;
+
+export async function get(path: string, params?: { [key: string]: string }) {
+  const paramsStr = params && '?' + new URLSearchParams(params).toString();
+  const response = await fetch(`https://api.ravelry.com${path}${paramsStr || ''}`, {
     headers: {
       'Authorization': btoa(`${VITE_RAVELRY_USERNAME}:${VITE_RAVELRY_PASSWORD}`),
     },
