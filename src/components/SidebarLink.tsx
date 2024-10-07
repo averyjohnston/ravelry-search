feba7c4b842b3c9cf6ae3d6ef363cecd22fc2208
averyjohnston <averyejohnston@gmail.com>
2024-10-07 14:23:12 -0500

    refactor: pull sidebar link styles into their own file

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -1,14 +1,18 @@
 import type { PropsWithChildren } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './SidebarLink.scss';
+
 export default function SidebarLink(props: PropsWithChildren<{
   to: string
 }>) {
   const { to, children } = props;
 
   return (
-    <NavLink to={to} className={
-      ({ isActive, isPending }) => isActive ? 'active' : isPending ? 'pending' : ''
-    }>{children}</NavLink>
+    <NavLink to={to} className={({ isActive, isPending }) => {
+      const defaultClass = 'sidebar-link';
+      const stateClass = isActive ? 'active' : isPending ? 'pending' : '';
+      return `${defaultClass} ${stateClass}`;
+    }}>{children}</NavLink>
   )
 }
