1794e2d19cd03d43508ce87d9110ff89827c792a
averyjohnston <averyejohnston@gmail.com>
2024-10-09 12:15:33 -0500

    feat(QueueSortPage): load and sort queue data (not displayed yet)

--- src/pages/QueueSortPage.tsx
+++ src/pages/QueueSortPage.tsx
@@ -4,7 +4,49 @@ import { useLoaderData } from 'react-router-dom';
 import { get, USERNAME } from '../utils';
 
 async function loader() {
-  return await get(`/people/${USERNAME}/queue/list.json`, { page_size: '500' });
+  const searchParams = {
+    page_size: '500',
+    queuer: USERNAME || '',
+  };
+
+  /**
+   * Making three calls like this isn't great, but it's necessary for three reasons:
+   * 1. Neither of the endpoints include the craft in the list.
+   * 2. The queue/list endpoint doesn't allow searching by craft.
+   * 3. The patterns/search endpoint doesn't sort by queue order.
+   *
+   * Doing it this way lets us combine craft searching with proper queue order. The
+   * next best method would be getting the queue list, then hitting the patterns/show
+   * endpoint for every single entry to retrieve the craft.
+   */
+  const [queueResult, crochetResult, knittingResult] = await Promise.all([
+    get(`/people/${USERNAME}/queue/list.json`, { page_size: searchParams.page_size }),
+    get('/patterns/search.json', {
+      ...searchParams,
+      craft: 'crochet',
+    }),
+    get('/patterns/search.json', {
+      ...searchParams,
+      craft: 'knitting',
+    }),
+  ]);
+
+  const orderedQueue = queueResult.queued_projects;
+  const crochetPatterns = crochetResult.patterns;
+  const knittingPatterns = knittingResult.patterns;
+
+  const orderedIDs = orderedQueue.map(entry => entry.pattern_id);
+
+  const sortPatterns = (patterns) => {
+    const sorted = patterns.slice();
+    sorted.sort((a, b) => orderedIDs.indexOf(a.id) - orderedIDs.indexOf(b.id));
+    return sorted;
+  };
+
+  return {
+    crochet: sortPatterns(crochetPatterns),
+    knitting: sortPatterns(knittingPatterns),
+  };
 }
 
 export default function QueueSortPage() {
