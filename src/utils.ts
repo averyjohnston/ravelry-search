819f1c23e22186bfce665897dd7258df77dbb0ed
averyjohnston <averyejohnston@gmail.com>
2024-10-17 21:10:06 -0500

    chore: todos

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ const { VITE_RAVELRY_USERNAME, VITE_RAVELRY_PASSWORD, VITE_RAVELRY_ACCOUNT_NAME 
 
 export const USERNAME = VITE_RAVELRY_ACCOUNT_NAME as string | undefined;
 
+// TODO: implement caching https://www.ravelry.com/api#extra_etags
+
 export async function get(path: string, params?: { [key: string]: string }) {
   const paramsStr = params && '?' + new URLSearchParams(params).toString();
   const response = await fetch(`https://api.ravelry.com${path}${paramsStr || ''}`, {
@@ -10,6 +12,7 @@ export async function get(path: string, params?: { [key: string]: string }) {
     },
   });
 
-  // TODO: types?
+  // types should be handled on the caller side
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return await response.json();
 }
