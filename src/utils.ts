b48bb4b25d9ff526d600b699b836ffa6be1aa9aa
averyjohnston <averyejohnston@gmail.com>
2024-10-24 20:43:43 -0500

    chore: remove todo (API doesn't look to be configured right, can't access etag header)

--- src/utils.ts
+++ src/utils.ts
@@ -5,8 +5,6 @@ const { VITE_RAVELRY_USERNAME, VITE_RAVELRY_PASSWORD, VITE_RAVELRY_ACCOUNT_NAME 
 export const USERNAME = VITE_RAVELRY_ACCOUNT_NAME as string | undefined;
 export const DEFAULT_IMAGE = 'https://www.ravelry.com/images/assets/illustrations/color/svg/blank-skein-herdwick.svg?v=6';
 
-// TODO: implement caching https://www.ravelry.com/api#extra_etags
-
 export async function get(path: string, params?: { [key: string]: string }) {
   const paramsStr = params && '?' + new URLSearchParams(params).toString();
   const response = await fetch(`https://api.ravelry.com${path}${paramsStr || ''}`, {
